e74ac7af4a5f675675270607c21b35b5ce485218
yfooks <yfooks@gmail.com>
2019-06-29 18:04:53 +0300

    url validation

--- src/app/features/videos/add-video-dialog/add-video-dialog.component.ts
+++ src/app/features/videos/add-video-dialog/add-video-dialog.component.ts
@@ -1,5 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-add-video-dialog',
@@ -8,13 +11,24 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 })
 export class AddVideoDialogComponent implements OnInit {
   url: string;
+  urlForm: FormGroup;
+  urlSub: Subscription;
 
-  constructor(
+  constructor(private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<AddVideoDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
   ngOnInit() {
+    const reg = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
+    this.urlForm = this.formBuilder.group({
+      url: ['', [Validators.required, Validators.pattern(reg)]],
+    });
+
+    this.urlSub = this.urlForm.get('url').valueChanges.pipe(debounceTime(500))
+      .subscribe(() => {
+        this.data.url = this.urlForm.get('url').value;
+      });
   }
 
   // onNoClick(): void {
